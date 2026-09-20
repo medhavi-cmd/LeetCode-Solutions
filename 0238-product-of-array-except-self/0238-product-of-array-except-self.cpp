b364e2594dcd9486dba5750cfd7fe2defda6be35
Medhavi Singh <medhavisingh270505@gmail.com>
2026-09-20 17:10:47 +0530

    Time: 0 ms (100%), Space: 40.3 MB (43.55%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -6,16 +6,13 @@ public:
         int prefix = 1;
         for(int i = 0; i<n; i++){
             answer[i] = prefix;
-            prefix *=nums[i];
+            prefix *= nums[i];
         }
-
         int suffix = 1;
         for(int i = n-1; i>=0; i--){
-            answer[i] *=suffix;
+            answer[i] *= suffix;
             suffix *= nums[i];
         }
-            
-        
         return answer;
     }
 };
